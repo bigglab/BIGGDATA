f8fa661220a558399266aebb7884aa476a549f83
Russell Durrett <russdurrett@gmail.com>
2015-11-16 14:06:20 -0600

    postgres sequence data, annotation, analysis added

--- app.py
+++ app.py
@@ -76,7 +76,8 @@ class User(db.Model):
         scratch_path = db.Column(db.String(256))
         files = db.relationship('File', backref='user', lazy='dynamic')
         datasets = db.relationship('Dataset', backref='user', lazy='dynamic')
-
+        analyses = db.relationship('Analysis', backref='user', lazy='dynamic')
+        
         def get_id(self):
             """Return the email address to satisfy Flask-Login's requirements."""
             return self.email
@@ -130,12 +131,14 @@ class File(db.Model):
         paired_partner = db.Column(db.Integer, db.ForeignKey('file.id'))
         parent = db.Column(db.Integer, db.ForeignKey('file.id'))
 
+        sequences = db.relationship('Sequence', backref='file', lazy='dynamic')
+
         def __repr__(self): 
             if self.paired_partner: 
                 p = 'Paired To: {}'.format(str(self.paired_partner))
             else: 
                 p = ''
-            return "<  File {}: _{}_  {}  {} >".format(self.id, self.file_type, self.name, p)
+            return "<File {}: _{}_  {}  {}>".format(self.id, self.file_type, self.name, p)
 
 
         def pair(self, f): 
@@ -160,6 +163,14 @@ class Dataset(db.Model):
         ig_type = db.Column(db.String(128))
         paired = db.Column(db.Boolean, default=False)
         files = db.relationship('File', backref='dataset', lazy='dynamic')
+        sequences = db.relationship('Sequence', backref='dataset', lazy='dynamic')
+        analyses = db.relationship('Analysis', backref='dataset', lazy='dynamic')
+        annotations = db.relationship('Annotation', backref='dataset', lazy='dynamic')
+
+
+        def __repr__(self): 
+            return "<Dataset {} : {} : {}>".format(self.id, self.name, self.description)
+
 
 class Experiment(db.Model):
         __tablename__ = 'experiment'
@@ -220,6 +231,136 @@ def build_exp_from_dict(dict):
                 vetted_k = ''.format(vetted_k, c)
         setattr(ex, k.lower(), v)
     return ex
+
+
+
+
+
+
+class Sequence(db.Model):  
+
+        id = Column(Integer(), primary_key=True)
+        dataset_id = Column(Integer(), ForeignKey('dataset.id'))
+        file_id = Column(Integer(), ForeignKey('file.id'))
+        header = Column(String(100))
+        sequence = Column(String(500))
+        quality = Column(String(500))
+        annotations = db.relationship('Annotation', backref='sequence', lazy='dynamic')
+
+        def __repr__(self): 
+            return "< Sequence {}: {} >".format(self.id, self.header)
+
+
+
+class Annotation(db.Model):  
+
+        id = Column(Integer(), primary_key=True)
+        sequence_id = Column(Integer(), ForeignKey('sequence.id'))
+        dataset_id = Column(Integer(), ForeignKey('dataset.id'))
+        analysis_id = Column(Integer(), ForeignKey('analysis.id'))
+        _id = Column(JSON())
+        seq_id = Column(JSON())
+        exp_id = Column(JSON())
+        analysis_name = Column(String())
+        recombination_type = Column(String())
+        strand = Column(String(10))
+        
+        productive = Column(db.Boolean)
+        productive_comment = Column(String(256))
+        recombination_type = Column(String(20))
+        chain = Column(String(20))
+
+        locus = Column(String)
+        isotype = Column(String)
+        isotype_mismatches = Column(Integer)
+        isotype_percent_similarity = Column(FLOAT)
+        isotype_barcode_direction = Column(String)
+
+        nt = Column(String(600))
+        aa = Column(String(200))
+
+        cdr1_aa = Column(String(100))
+        cdr1_nt = Column(String(100))
+        cdr2_aa = Column(String(100))
+        cdr2_nt = Column(String(100))
+        cdr3_aa = Column(String(100))
+        cdr3_nt = Column(String(100))
+        fr1_nt = Column(String(200))
+        fr1_aa = Column(String(100))
+        fr2_nt = Column(String(100))
+        fr2_aa = Column(String(100))
+        fr3_nt = Column(String(100))
+        fr3_aa = Column(String(200))
+        fr4_nt  = Column(String(100))
+        fr4_aa = Column(String(100))
+
+        c_top_hit = Column(String) 
+        c_top_hit_locus = Column(String)
+        v_top_hit = Column(String) 
+        v_top_hit_locus = Column(String) 
+        d_top_hit = Column(String) 
+        d_top_hit_locus = Column(String) 
+        j_top_hit = Column(String) 
+        j_top_hit_locus = Column(String) 
+        
+        c_hits = Column(JSON)
+        j_hits = Column(JSON)
+        d_hits = Column(JSON)
+        v_hits = Column(JSON)
+
+        shm_aa = Column(FLOAT())
+        shm_nt = Column(FLOAT())
+        shm_nt_percent = Column(FLOAT())
+        shm_aa_percent = Column(FLOAT())
+        v_shm_nt = Column(Integer)
+        v_shm_percent = Column(FLOAT)
+        j_shm_nt = Column(Integer)
+        j_shm_percent = Column(FLOAT)
+
+        full_length = Column(Boolean)
+        cdr3_junction_in_frame = Column(Boolean)
+        codon_frame = Column(Integer)
+        start_codon = Column(Integer)
+        stop_codon = Column(Integer)
+
+        def __repr__(self): 
+            return "< Annotation {} on Sequence {} : {} >".format(self.id, self.sequence_id, self.analysis_name)
+
+
+
+
+class Analysis(db.Model):  
+
+        id = Column(Integer(), primary_key=True)
+        user_id = Column(Integer(), ForeignKey('user.id'))
+        dataset_id = Column(Integer(), ForeignKey('dataset.id'))
+        name = Column(String())
+        description = Column(String(256))
+        program = Column(String())
+        started = Column(TIMESTAMP)
+        finished = Column(TIMESTAMP)
+        duration = Column(TSRANGE)
+        params = Column(JSON)
+        vdj_count = Column(Integer)
+        vj_count = Column(Integer)
+        tcra_count = Column(Integer)
+        tcrb_count = Column(Integer)
+        total_count = Column(Integer)
+        passed_filter = Column(Integer)
+        command = Column(String(256))
+        notes = Column(String(1000))
+
+        annotations = db.relationship('Annotation', backref='analysis', lazy='dynamic')
+
+        def __repr__(self): 
+            return "< Analysis {}: {} : {} : {}>".format(self.id, self.program, self.name, self.started)
+
+
+
+
+
+
+
 
 def flatten_list(lst):
     return [item for sublist in lst for item in sublist]
