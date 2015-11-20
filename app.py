f325e276e4eaddd30f01b2bd8c4a0242ca64d4fa
Russell Durrett <russdurrett@gmail.com>
2015-11-20 08:41:21 -0600

    mixcr task fix

--- app.py
+++ app.py
@@ -1569,7 +1569,7 @@ def run_mixcr_analysis_id_with_files(analysis_id, files):
             analysis.status = 'FAILED'
             db.session.commit()
     print 'All commands in analysis {} have been executed.'.format(analysis)
-    if set(responses) == [0]:
+    if set(lambda f: f.available) == [True]:
         analysis.status = 'SUCCESS'
         analysis.available = True
     if not analysis.status == 'FAILED': analysis.status = 'SUCCESS'
