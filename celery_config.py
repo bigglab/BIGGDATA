f81d2bccda4c4c2c0401c2447bb1bd44b99621f7
Russell Durrett <russdurrett@gmail.com>
2016-03-29 14:33:08 -0500

    rabbitmq reverted

--- celery_config.py
+++ celery_config.py
@@ -47,12 +47,12 @@ CELERY_ROUTES = (
 
 
 # @Dave - temporary edit - using local RabbitMQ for testing
-BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+# BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 
-# if 'RABBITMQ_BIGWIG_URL' in os.environ.keys(): # On Heroku = Sender Queue: 
-# 	BROKER_URL = os.environ['RABBITMQ_BIGWIG_TX_URL']
-# if 'LESSOPEN' in os.environ.keys(): # AWS Processor = Reciever RX Queue: 
-# 	BROKER_URL="amqp://Sq_rhK2z:sGYEo2JbO0z3JpXh8YJq58NuMbvbM2Su@leaping-pipkin-62.bigwig.lshift.net:10919/thzgJ-IWDfsZ"
-# else: # Web Server Fired Anywhere Else: Sender Queue 
-# 	BROKER_URL = 'amqp://Sq_rhK2z:sGYEo2JbO0z3JpXh8YJq58NuMbvbM2Su@leaping-pipkin-62.bigwig.lshift.net:10918/thzgJ-IWDfsZ'
+if 'RABBITMQ_BIGWIG_URL' in os.environ.keys(): # On Heroku = Sender Queue: 
+	BROKER_URL = os.environ['RABBITMQ_BIGWIG_TX_URL']
+if 'LESSOPEN' in os.environ.keys(): # AWS Processor = Reciever RX Queue: 
+	BROKER_URL="amqp://Sq_rhK2z:sGYEo2JbO0z3JpXh8YJq58NuMbvbM2Su@leaping-pipkin-62.bigwig.lshift.net:10919/thzgJ-IWDfsZ"
+else: # Web Server Fired Anywhere Else: Sender Queue 
+	BROKER_URL = 'amqp://Sq_rhK2z:sGYEo2JbO0z3JpXh8YJq58NuMbvbM2Su@leaping-pipkin-62.bigwig.lshift.net:10918/thzgJ-IWDfsZ'
 
