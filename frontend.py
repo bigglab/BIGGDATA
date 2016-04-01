e7ae387a894c299c8ad7825021d08a682c743897
David Magness <david.m.magness@gmail.com>
2016-04-01 10:25:20 -0500

    Minor changes from previous commit.

--- frontend.py
+++ frontend.py
@@ -440,41 +440,6 @@ def pandaseq(dataset_id, status=[]):
     else: 
         return render_template("pandaseq.html", dataset=dataset, form=form, status=status) 
 
-# @frontend.route('/browse_experiments', methods=['GET', 'POST'])
-# @login_required
-# def browse_experiments():
-#     # print request.__dict__
-#     files = current_user.files.all()
-#     datasets = current_user.datasets.all()
-#     datadict = tree()
-#     for dataset in datasets:
-#         datadict[dataset] = dataset.files.all()
-#     form = Form()
-#     exps = db.session.query(Experiment).order_by(Experiment.curated.desc(), Experiment.experiment_creation_date.desc()).all()
-#     species_data = sorted(db.engine.execute('select species, count(*) from experiment GROUP BY species;').fetchall(), key=lambda x: x[1], reverse=True)
-#     chain_data = sorted(db.engine.execute('select chain_types_sequenced, count(*) from experiment GROUP BY chain_types_sequenced;').fetchall(), key=lambda x: x[1], reverse=True)
-#     cell_data = sorted(db.engine.execute('select cell_types_sequenced, count(*) from experiment GROUP BY cell_types_sequenced;').fetchall(), key=lambda x: x[1], reverse=True)
-#     primer_data = sorted(db.engine.execute('select primer_set_name, count(*) from experiment GROUP BY primer_set_name;').fetchall(), key=lambda x: x[1], reverse=True)
-#     cell_marker_data = sorted(db.engine.execute('select cell_markers_used, count(*) from experiment GROUP BY cell_markers_used;').fetchall(), key=lambda x: x[1], reverse=True)
-#     owner_query_data = sorted(db.engine.execute('select owners_of_experiment, count(*) from experiment GROUP BY owners_of_experiment;').fetchall(), key=lambda x: x[1], reverse=True)
-#     owners = set(flatten_list([o[0] for o in owner_query_data]))
-#     owner_data = {}
-#     for o in owners: 
-#         owner_data[o] = 0 
-#     for os,c in owner_query_data: 
-#         for o in os: 
-#             owner_data[o] += c 
-#     owner_data = sorted(owner_data.items(), key=operator.itemgetter(1), reverse=True)
-#     isotype_query_data = sorted(db.engine.execute('select isotypes_sequenced, count(*) from experiment GROUP BY isotypes_sequenced;').fetchall(), key=lambda x: x[1], reverse=True)
-#     isotype_data = demultiplex_tuple_counts(isotype_query_data)
-#     # sorted(isotype_counts.items(), key=operator.itemgetter(0))
-#     cell_marker_query_data = sorted(db.engine.execute('select cell_markers_used, count(*) from experiment GROUP BY cell_markers_used;').fetchall(), key=lambda x: x[1], reverse=True)
-#     cell_marker_data=demultiplex_tuple_counts(cell_marker_query_data, index=1, reverse=True)
-#     # print cell_marker_data 
-#     golden = retrieve_golden()
-#     err = False
-#     return render_template("browse_experiments.html", datadict=datadict, form=form, exps=exps, err=err, gif_path=golden, species_data=species_data, chain_data=chain_data, cell_data=cell_data, cell_marker_data=cell_marker_data, primer_data=primer_data, isotype_data=isotype_data, owner_data=owner_data)
-
 @frontend.route('/browse_sequences', methods=['GET', 'POST'])
 @login_required
 def browse_sequences():
