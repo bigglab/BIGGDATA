8f3c5df4e4ea7995d5162b2cf5c8cface25ca43a
Russell Durrett <russdurrett@gmail.com>
2016-05-17 13:13:42 -0500

    commented config

--- instance/config.py
+++ instance/config.py
@@ -60,74 +60,31 @@ IGREP_PIPELINES = '/data/resources/software/IGREP/pipelines'
 
 # Set config based on computer name
 # Development only
-import subprocess
-import shlex
+# import subprocess
+# import shlex
 
-DAVES_MACHINE = False
+# DAVES_MACHINE = False
 
-try:
-	command_line_args = shlex.split('scutil --get LocalHostName')
-	command_line_process = subprocess.Popen( command_line_args , stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	response, error = command_line_process.communicate()
-	if 'Davids-MacBook-Air' in response:
-		DAVES_MACHINE = True
-except subprocess.CalledProcessError as error:
-    error = error.output
+# try:
+# 	command_line_args = shlex.split('scutil --get LocalHostName')
+# 	command_line_process = subprocess.Popen( command_line_args , stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+# 	response, error = command_line_process.communicate()
+# 	if 'Davids-MacBook-Air' in response:
+# 		DAVES_MACHINE = True
+# except subprocess.CalledProcessError as error:
+#     error = error.output
 
-if DAVES_MACHINE:
-	IGREP_COMMON_TOOLS = '/Users/davidmagness/IGREP/common_tools'
-	IGREP_PIPELINES = '/Users/davidmagness/IGREP/pipelines'
+# if DAVES_MACHINE:
+# 	IGREP_COMMON_TOOLS = '/Users/davidmagness/IGREP/common_tools'
+# 	IGREP_PIPELINES = '/Users/davidmagness/IGREP/pipelines'
 
-	SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/biggdata"
-	#SQLALCHEMY_DATABASE_URI = "postgres://biggdata:jkl@127.0.0.1:6712/biggdata"
-	SQLALCHEMY_TRACK_MODIFICATIONS = True
-	SQLALCHEMY_POOL_TIMEOUT = None
+# 	SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/biggdata"
+# 	#SQLALCHEMY_DATABASE_URI = "postgres://biggdata:jkl@127.0.0.1:6712/biggdata"
+# 	SQLALCHEMY_TRACK_MODIFICATIONS = True
+# 	SQLALCHEMY_POOL_TIMEOUT = None
 
-	TRIMMOMATIC = 'trimmomatic'
-	TRIMMOMATIC_ADAPTERS = '/usr/local/share/trimmomatic/adapters'
-
-# OPTIONS AVAILABLE ACCORDING TO app.config.__dict__  AFTER COMPILATION 
-
- # 'BOOTSTRAP_LOCAL_SUBDOMAIN': None, 
- # 'BOOTSTRAP_USE_MINIFIED': True, 
- # 'SESSION_COOKIE_PATH': None, 
- # 'SQLALCHEMY_RECORD_QUERIES': None, 
- # 'SESSION_COOKIE_DOMAIN': None, 
- # 'SESSION_COOKIE_NAME': 'session', 
- # 'BOOTSTRAP_SERVE_LOCAL': False, 
- # 'SQLALCHEMY_BINDS': None, 
- # 'SQLALCHEMY_POOL_RECYCLE': None, 
- # 'BOOTSTRAP_CDN_FORCE_SSL': False, 
- # 'LOGGER_NAME': 'app', 'DEBUG': True, 
- # 'SQLALCHEMY_COMMIT_ON_TEARDOWN': False, 
- # 'SECRET_KEY': '\x95\x90+\x1c\xd36\xa3\x94\x99\xaeA\xac\xd3M5\x0b\xc7\xefF\xf3\x08\x05t\xd9', 
- # 'SQLALCHEMY_NATIVE_UNICODE': None, 
- # 'MAX_CONTENT_LENGTH': None, 
- # 'SQLALCHEMY_POOL_SIZE': None, 
- # 'SQLALCHEMY_ECHO': False, 
- # 'APPLICATION_ROOT': None, 
- # 'SERVER_NAME': None, 
- # 'PREFERRED_URL_SCHEME': 'http', 
- # 'JSONIFY_PRETTYPRINT_REGULAR': True, 
- # 'TESTING': False, 
- # 'PERMANENT_SESSION_LIFETIME': datetime.timedelta(31), 
- # 'PROPAGATE_EXCEPTIONS': None, 
- # 'TRAP_BAD_REQUEST_ERRORS': False, 
- # 'JSON_SORT_KEYS': True, 
- # 'SQLALCHEMY_MAX_OVERFLOW': None, 
- # 'SESSION_COOKIE_HTTPONLY': True, 
- # 'SEND_FILE_MAX_AGE_DEFAULT': 43200, 
- # 'PRESERVE_CONTEXT_ON_EXCEPTION': None, 
- # 'S3_BUCKET': 's3://BIGGDATA',
- #  'SESSION_COOKIE_SECURE': False, 
- #  'TRAP_HTTP_EXCEPTIONS': False
-
-
-
-
-
-
-
+# 	TRIMMOMATIC = 'trimmomatic'
+# 	TRIMMOMATIC_ADAPTERS = '/usr/local/share/trimmomatic/adapters'
 
 
 
